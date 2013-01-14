6247fcdd55c1a4ccfff530e1da62a22808dd8b34
aharter <mail@andreas-harter.de>
2013-01-14 20:30:45 +0100

    use integrated kernel build

--- Android.mk
+++ Android.mk
@@ -1,13 +1,1 @@
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-endif
-
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-LOCAL_PATH := device/rpi/raspberrypi
-include $(CLEAR_VARS)
